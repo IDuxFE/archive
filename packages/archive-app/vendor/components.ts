617ca48f6a22d7b1c78793e09550f57443cb3f0f
sallerli1 <weskerli@163.com>
2023-06-16 15:08:31 +0800

    fix(app): components shouldn't export vue3 related component

--- packages/archive-app/vendor/components.ts
+++ packages/archive-app/vendor/components.ts
@@ -5,12 +5,7 @@
  * found in the LICENSE file at https://github.com/IDuxFE/archive/blob/main/LICENSE
  */
 
-import Demo from '../src/components/demo'
-
 export * from '@idux/components/tooltip'
 export * from '@idux/components/button'
 export * from '@idux/components/icon'
 export { createDemoInstance } from '../createDemoInstance'
-
-export const DemoComp = Demo
-export type { DemoProps } from '../src/components/demo/Demo'
